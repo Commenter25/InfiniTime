fe4ead9732b2cefc3f07bfd3f0078b3881520c45
Commenter <35016761+Commenter25@users.noreply.github.com>
2022-10-16 18:38:02 -0500

    Change name/source link in SystemInfo

--- src/displayapp/screens/FirmwareValidation.cpp
+++ src/displayapp/screens/FirmwareValidation.cpp
@@ -18,7 +18,7 @@ FirmwareValidation::FirmwareValidation(Pinetime::Applications::DisplayApp* app, 
   : Screen {app}, validator {validator} {
   labelVersion = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_fmt(labelVersion,
-                        "Version : %lu.%lu.%lu\n"
+                        "Version : %lu.%lu.%lu [25]\n"
                         "ShortRef : %s",
                         Version::Major(),
                         Version::Minor(),
